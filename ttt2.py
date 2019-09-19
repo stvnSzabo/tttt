2a889bd7112d9889e59c08b64986c246df64be9c
stvnSzabo <55262494+stvnSzabo@users.noreply.github.com>
2019-09-19 11:56:00 +0200

    Update ttt2.py

--- ttt2.py
+++ ttt2.py
@@ -1,22 +1,24 @@
-project_board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+project_board = ['1','2','3','4','5','6','7','8','9']
 
 
 def printboard():
     print('\n'*2)
-    print('-- --- ---')
-    print( project_board[0] + ' | ' + project_board[1] + ' | ' + project_board[2])
-    print('-- --- ---')
-    print( project_board[3] + ' | ' + project_board[4] + ' | ' + project_board[5])
-    print('-- --- ---')
-    print( project_board[6] + ' | ' + project_board[7] + ' | ' + project_board[8])
-    print('-- --- ---')
+    print(' -------------')
+    print( ' | ' + project_board[0] + ' | ' + project_board[1] + ' | ' + project_board[2] + ' | ')
+    print(' -------------')
+    print( ' | ' +  project_board[3] + ' | ' + project_board[4] + ' | ' + project_board[5] + ' | ')
+    print(' -------------')
+    print( ' | ' +  project_board[6] + ' | ' + project_board[7] + ' | ' + project_board[8] + ' | ')
+    print(' -------------')
     print('\n')
+
+
 
 def setplayer():
     player1 = ''
     player2 = ''
     while player1 == '' and player2 == '':
-        ques = input('Player1, do you want to be X or O?').upper()
+        ques = input('Player1, do you want to be X or O? ').upper()
         if ques == 'X':
             player1 = 'X'
             player2 = 'O'
@@ -29,6 +31,16 @@ def setplayer():
 
 player1,player2 = setplayer()
 
+print('\n'*2)
+print(' -------------')
+print( ' | ' + project_board[0] + ' | ' + project_board[1] + ' | ' + project_board[2] + ' | ')
+print(' -------------')
+print( ' | ' +  project_board[3] + ' | ' + project_board[4] + ' | ' + project_board[5] + ' | ')
+print(' -------------')
+print( ' | ' +  project_board[6] + ' | ' + project_board[7] + ' | ' + project_board[8] + ' | ')
+print(' -------------')
+print('\n')
+
 
 
 def game():
@@ -37,28 +49,28 @@ def game():
     while True:
         try:
             if x == o:
-                ques = int(input('Player1, choose a number between 1-9 or 11 to quit \n'))
+                ques = int(input('Player1, choose a number between 1-9 or 11 to quit \n '))
                 if ques <= 9 and ques >= 1:
                     check = (ques - 1)
-                    if project_board[check] == ' ':
+                    if project_board[check] == '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9':
                         project_board[check] = player1
                         x += 1
                     else:
-                        print(f'{player2} is already in that location! Try another.')
+                        print(f'{player2} is already in that location! Try another. ')
                         continue
 
                 elif ques == 11:
                      print(quit())
                      break
                 else:
-                    print('Please choose a number between 1-9 or 11 to quit')
+                    print('Please choose a number between 1-9 or 11 to quit ')
                     continue
 
             else:
-                ques = int(input('Player2, choose a number between 1-9 or 11 to quit \n'))
+                ques = int(input('Player2, choose a number between 1-9 or 11 to quit \n '))
                 if ques <= 9 >= 1:
                     check = (ques - 1)
-                    if project_board[check] == ' ':
+                    if project_board[check] == '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9':
                         project_board[check] = player2
                         x -= 1
                     else:
@@ -70,7 +82,7 @@ def game():
                      break
                 
                 else:
-                    print('Please choose a number between 1-9 or 11 to quit')
+                    print('Please choose a number between 1-9 or 11 to quit ')
                     continue
                     
 
@@ -78,33 +90,32 @@ def game():
             print('Please choose a number between 1-9')
             continue
         
-        if project_board[0] == project_board[1] == project_board[2] and project_board[0] != ' ':
+        if project_board[0] == project_board[1] == project_board[2] and project_board[0] != '1':
             winner = f'{project_board[0]} is the winner!'
             break
-        elif project_board[6] == project_board[7] == project_board[8] and project_board[6] != ' ':
+        elif project_board[6] == project_board[7] == project_board[8] and project_board[6] != '7':
             winner = f'{project_board[6]} is the winner!'
             break
-        elif project_board[2] == project_board[5] == project_board[8] and project_board[2] != ' ':
+        elif project_board[2] == project_board[5] == project_board[8] and project_board[2] != '3':
             winner = f'{project_board[2]} is the winner!'
             break
-        elif project_board[0] == project_board[4] == project_board[8] and project_board[0] != ' ':
+        elif project_board[0] == project_board[4] == project_board[8] and project_board[0] != '1':
             winner = f'{project_board[0]} is the winner!'
             break
-        elif project_board[2] == project_board[4] == project_board[6] and project_board[2] != ' ':
+        elif project_board[2] == project_board[4] == project_board[6] and project_board[2] != '3':
             winner = f'{project_board[2]} is the winner!'
             break
-        elif project_board[3] == project_board[4] == project_board[5] and project_board[3] != ' ':
+        elif project_board[3] == project_board[4] == project_board[5] and project_board[3] != '4':
             winner = f'{project_board[3]} is the winner!'
             break
-        elif project_board[0] == project_board[3] == project_board[6] and project_board[0] != ' ':
+        elif project_board[0] == project_board[3] == project_board[6] and project_board[0] != '1':
             winner = f'{project_board[0]} is the winner!'
             break
-        elif project_board[1] == project_board[4] == project_board[7] and project_board[1] != ' ':
+        elif project_board[1] == project_board[4] == project_board[7] and project_board[1] != '2':
             winner = f'{project_board[1]} is the winner!'
             break
-        elif ' ' not in project_board:
+        elif '1' and '2' and '3' and '4' and '5' and '6' and '7' and '8' and '9' not in project_board:
             winner = 'Nobody won. Tie game!'
-            print('Nobody won. Tie game!')
             break
         printboard()
     printboard()

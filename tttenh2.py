06faed1eb124f29806807be534b643ac9e700f29
stvnSzabo <55262494+stvnSzabo@users.noreply.github.com>
2019-10-03 09:57:23 +0200

    Update tttenh2.py

--- tttenh2.py
+++ tttenh2.py
@@ -2,12 +2,22 @@ import sys
 import os
 import datetime
 
+
 project_board = ['1','2','3','4','5','6','7','8','9']
 
 
 start = datetime.datetime.now()
 
+print('''888   d8b        888                   888                    
+888   Y8P        888                   888                    
+888              888                   888                    
+888888888 .d8888b888888 8888b.  .d8888b888888 .d88b.  .d88b.  
+888   888d88P"   888       "88bd88P"   888   d88""88bd8P  Y8b 
+888   888888     888   .d888888888     888   888  88888888888 
+Y88b. 888Y88b.   Y88b. 888  888Y88b.   Y88b. Y88..88PY8b.     
+ "Y888888 "Y8888P "Y888"Y888888 "Y8888P "Y888 "Y88P"  "Y8888  ''')
 
+print('\n'*2)
 
 def printboard():
     print('\n'*2)
@@ -141,6 +151,8 @@ def game():
         printboard()
     printboard()
     print(winner)
+    
+
 
 
 
